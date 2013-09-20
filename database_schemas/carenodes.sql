4291fac01891510a34ca3ecb081ff339b3b9da05
Leask Huang <i@leaskh.com>
2013-09-21 04:13:48 +0800

    Freezed @ Sat Sep 21 04:13:48 CST 2013

--- database_schemas/carenodes.sql
+++ database_schemas/carenodes.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE `carenodes`;
 
-grant all privileges on carenodes.* to 'carenodes'@'%' identified by '689270b764b555a88deda40bcb2c0d44';
+-- grant all privileges on carenodes.* to 'carenodes'@'%' identified by '689270b764b555a88deda40bcb2c0d44';
+grant all privileges on carenodes.* to 'carenodes'@'localhost' identified by '689270b764b555a88deda40bcb2c0d44';
 FLUSH PRIVILEGES;
